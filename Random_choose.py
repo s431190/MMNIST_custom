6b21642db4cf9d1966dded729ae069ccb04c7a4e
s431190 <s431190@gmail.com>
2023-01-05 15:16:06 +0800

    January_v1

--- Random_choose.py
+++ Random_choose.py
@@ -12,18 +12,18 @@ def moveFile(fileDir, tarDir):
         shutil.move(fileDir + name, tarDir + name)
         
 path1 = r'./new_dataset/'
-floder1 = r'train',r'test'
-floder2 = r'0', r'1', r'2', r'3', r'4', r'5', r'6', r'7', r'8', r'9'   
+folder1 = r'train',r'test'
+folder2 = r'0', r'1', r'2', r'3', r'4', r'5', r'6', r'7', r'8', r'9'   
        
 if __name__ == '__main__':
     
-    for name1 in floder1:
-        for name2 in floder2:    
+    for name1 in folder1:
+        for name2 in folder2:    
             if not os.path.exists(os.path.join(path1 + name1, name2)):
                  os.makedirs(os.path.join(path1 + name1, name2))
                  
-    for name3 in floder1:
-        for name4 in floder2:
+    for name3 in folder1:
+        for name4 in folder2:
             fileDir = './' + name3 + '/' + name4 + '/' 
             tarDir = path1 + name3 + '/' + name4 + '/' 
             moveFile(fileDir, tarDir)
